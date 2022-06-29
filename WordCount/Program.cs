3503cd1c9ef08babf65599945d51aa0fb6d00020
TECHRITOMA Inc <ajuatah@gmail.com>
2022-06-29 12:19:00 +0100

    Add project files.

---
+++ WordCount/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = " a text, some text , just some text";
+
+            IDictionary<string , int> myDictionatry = new Dictionary<string , int>();
+
+            string[] words = text.Split(' ', ',', '.') ;
+
+            foreach (string word in words)
+            {
+                int count = 1;
+                if (myDictionatry.ContainsKey(word))
+                {
+                    count = myDictionatry[word] +1;
+                    
+                } else
+                {
+                   myDictionatry[word] =1;
+
+                }
+
+            }
+
+            foreach(var pair in myDictionatry)
+            {
+                Console.WriteLine($"{pair.Key} ==> {pair.Value}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
